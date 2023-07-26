50d7d72d0c8d0910cf9879f74d3b6c47c1e26198
= <jwparker284@gmail.com>
2023-07-25 19:16:07 -0700

    Mapped data from array, and display data

--- puppyPals/puppy-pals/src/App.jsx
+++ puppyPals/puppy-pals/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from "react"
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppyList);
 
   return (
     <div>
-      
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+
+      })}
+
     </div>
   )
 }
