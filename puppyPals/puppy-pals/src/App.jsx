5d3b0952c35f96b7486c00b288f817cf67c828f9
= <jwparker284@gmail.com>
2023-07-27 05:08:05 -0700

    Main component complete, and displays all desired information

--- puppyPals/puppy-pals/src/App.jsx
+++ puppyPals/puppy-pals/src/App.jsx
@@ -5,14 +5,28 @@ import { puppyList } from './data'
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
-
+        return <p onClick={()=>{setFeatPupId(puppy.id)
+          
+          console.log(featuredPup)
+        }
+        }key={puppy.id}>{puppy.name}
+        </p>
       })}
-
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
