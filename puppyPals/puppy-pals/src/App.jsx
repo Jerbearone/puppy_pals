37de27cce9ae0c81e42d72c4da9a7d3d1758201f
= <jwparker284@gmail.com>
2023-07-27 06:08:16 -0700

    updated styling for flexbox to align elements for a better UX

--- puppyPals/puppy-pals/src/App.jsx
+++ puppyPals/puppy-pals/src/App.jsx
@@ -9,24 +9,35 @@ function App() {
   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   return (
-    <div>
-      {puppies.map((puppy) => {
-        return <p className="puppy_name_component" onClick={()=>{setFeatPupId(puppy.id)
-          
-          console.log(featuredPup)
-        }
-        }key={puppy.id}>{puppy.name}
-        </p>
-      })}
-      {featPupId && (
-        <div>
-          <h2 className="featured_puppy_name">{featuredPup.name}</h2>
-          <ul className="featured_puppy_list">
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
+    <div className="main_div">
+      <div className="puppy_name_list">
+
+        {puppies.map((puppy) => {
+          return <p className="puppy_names_view" onClick={()=>{setFeatPupId(puppy.id)
+            
+            console.log(featuredPup)
+          }
+          }key={puppy.id}>{puppy.name}
+          </p>
+        })}
+
+
+      </div> 
+
+      <div className="featured_puppy_view">
+
+        {featPupId && (
+          <div>
+            <h2 className="featured_puppy_name">{featuredPup.name}</h2>
+            <ul className="featured_puppy_list">
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+
+
+      </div>
     </div>
   )
 }
